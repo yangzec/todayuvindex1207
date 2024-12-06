890c8e9f901dba00e020b386f75c29833b640825
yangzec <yangzec@gmail.com>
2024-12-07 02:57:21 +0800

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { LineText } from "@/components/LineText";
-import CTAButton from "@/components/home/CTAButton";
 
 const Hero = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
   return (
@@ -20,6 +19,16 @@ const Hero = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
         }}
       > */}
 
+      
+<section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-16 md:pt-2 text-center">
+        <h1>
+          {locale.title1} <LineText>{locale.title2}</LineText> {locale.title3}
+        </h1>
+        <p className="mx-auto mt-6 max-w-2xl text-2xl tracking-tight text-slate-700 dark:text-slate-400">
+          {/* {siteConfig.description} */}
+          {locale.description}
+        </p>
+      </section>
       <section className="weather-widget-container">
         <div>
           <iframe
@@ -32,17 +41,8 @@ const Hero = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
           ></iframe>
         </div>
       </section>
-      <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-16 md:pt-24 text-center">
-        <h1>
-          {locale.title1} <LineText>{locale.title2}</LineText> {locale.title3}
-        </h1>
-        <p className="mx-auto mt-6 max-w-2xl text-2xl tracking-tight text-slate-700 dark:text-slate-400">
-          {/* {siteConfig.description} */}
-          {locale.description}
-        </p>
-      </section>
       {/* </motion.div> */}
-      <CTAButton locale={CTALocale}></CTAButton>
+      {/* <CTAButton locale={CTALocale}></CTAButton> */}
     </>
   );
 };
